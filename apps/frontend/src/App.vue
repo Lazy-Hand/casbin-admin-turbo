304761f7e0f0a19e4e83eb6740b9bcc242728868
wujiatong <17662073174@163.com>
2026-03-30 11:31:51 +0800

    fix: 更新数据源配置，修正 PostgreSQL 连接信息

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import AppMain from './AppMain.vue'
-import NaiveProvider from './components/Naive/NaiveProvider.vue'
-import AppLoading from './components/Naive/AppLoading.vue'
-import { PiniaColadaDevtools } from '@pinia/colada-devtools'
+import AppMain from "./AppMain.vue";
+import NaiveProvider from "./components/Naive/NaiveProvider.vue";
+import AppLoading from "./components/Naive/AppLoading.vue";
+import { PiniaColadaDevtools } from "@pinia/colada-devtools";
 // 使用 Suspense 处理异步组件加载
 </script>
 
